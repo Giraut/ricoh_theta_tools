b1f44064f7243ca47c3482948084b96d1ec49347
Giraut <>
2026-09-22 02:50:41 +0300

    Added automatic nighttime / daytime camera reconfiguration based on nautical dusk / dawn times at the location reported by the camera's GPS receiver

--- theta_grab_timelapse_images.py
+++ theta_grab_timelapse_images.py
@@ -4,12 +4,15 @@
 
 ### Modules
 import os
+import re
 import sys
 import json
 import argparse
 from thetaclass.theta import Theta
 from time import time, sleep
-from datetime import datetime
+from datetime import datetime, timedelta
+from astral import Observer, Depression
+from astral.sun import dawn, dusk, elevation
 
 try:
   import argcomplete	# PYTHON_ARGCOMPLETE_OK
@@ -22,8 +25,9 @@ except:
 grab_image_every = 30 #s
 retries = 20
 wait_before_retry = 2 #s
-monitor_camera_state = True
 reconnect_tries = 5
+
+twilight_depression = Depression.NAUTICAL.value # sun ~12 deg below the horizon
 
 # Theta cameras' names and credentials file
 #
@@ -53,8 +57,41 @@ ERROR = 2
 
 
 
-
 ### Routines
+def configure_camera_for_daytime_capture(rt):
+  """Configure the camera to take daytime pictures
+  """
+
+  log(INFO, "Daytime camera setup: exposure program = auto")
+  retry(rt, rt.set_exposure_program, "auto", reconnect_tries = reconnect_tries)
+
+  log(INFO, "Daytime camera setup: filter = hdr")
+  retry(rt, rt.set_filter, "hdr", reconnect_tries = reconnect_tries)
+
+  log(INFO, "Daytime camera setup: EV compensation = 0")
+  retry(rt, rt.set_exposure_compensation, 0, reconnect_tries = reconnect_tries)
+
+
+
+def configure_camera_for_nighttime_capture(rt):
+  """Configure the camera to take daytime pictures
+  """
+
+  log(INFO, "Nighttime camera setup: exposure program = manual")
+  retry(rt, rt.set_exposure_program, "manual",
+					reconnect_tries = reconnect_tries)
+
+  log(INFO, "Nighttime camera setup: shutter_speed = 10s")
+  retry(rt, rt.set_shutter_speed, 10, reconnect_tries = reconnect_tries)
+
+  log(INFO, "Nighttime camera setup: ISO sensitivity = 800")
+  retry(rt, rt.set_iso_sensitivity, 800, reconnect_tries = reconnect_tries)
+
+  log(INFO, "Nighttime camera setup: EV compensation = 0")
+  retry(rt, rt.set_exposure_compensation, 0, reconnect_tries = reconnect_tries)
+
+
+
 def theta_camera_names_completer(**kwargs):
   """Argcomplete completer that returns all the names of the cameras declared in
   the Theta cameras' names and credentials file
@@ -163,9 +200,17 @@ def main():
   # Open the camera
   rt = Theta(**theta_cameras_credentials[args.camera])
 
-  # Assume the camera needs setting up when starting
-  do_setup = True
+  # Assume the camera needs setting up when starting, and we don't know whether
+  # capture parameters are set for daytime or nighttime, but it doesn't need
+  # rebooting at this point
+  do_basic_setup = True
+  camera_set_for_daytime = None
   do_reboot = False
+
+  # Regex to match the GPS date/time/timezone returned in the camera's state
+  gps_datetimezone_regex = re.compile(r"^([0-9]{4}):([0-9]{2}):([0-9]{2})\s" \
+					r"([0-9]{2}):([0-9]{2}):([0-9]{2})" \
+					"([+-][0-9]{1,2}:[0-9]{2})$")
 
   # Grab images forever
   while True:
@@ -179,20 +224,25 @@ def main():
         rt.reboot()
 
         do_reboot = False
-        do_setup = True
+        do_basic_setup = True
+        camera_set_for_daytime = None
 
-      # Should we setup the camera?
-      if do_setup:
+      # Should we do a basic setup of the camera?
+      if do_basic_setup:
 
-        log(INFO, "Setting up the camera: power mode = silent")
+        log(INFO, "Basic camera setup: power mode = silent")
         retry(rt, rt.set_power_mode, "silent",
 		reconnect_tries = reconnect_tries)
 
-        log(INFO, "Setting up the camera: capture mode = image")
+        log(INFO, "Basic camera setup: GPS tag recording = on")
+        retry(rt, rt.set_power_mode, "silent",
+		reconnect_tries = reconnect_tries)
+
+        log(INFO, "Basic camera setup: capture mode = image")
         retry(rt, rt.set_capture_mode, "image",
 		reconnect_tries = reconnect_tries)
 
-        do_setup = False
+        do_basic_setup = False
         next_shot_tstamp = time()
 
       # Wait until the next shot, if needed
@@ -213,24 +263,119 @@ def main():
           log(WARN, "Capping the lateness to {:0.1f} s".
 			format(grab_image_every))
 
-      # Get the battery's SoC, temperature, PCB temperature and camera errors
-      if monitor_camera_state:
-        state = retry(rt, rt.get_state, reconnect_tries = reconnect_tries)
+      # We don't know whether it's daytime or nighttime yet without GPS data
+      is_daytime = None
 
-        batt_percent = state["batteryLevel"] * 100
-        batt_state = state["_batteryState"]
-        batt_temp = state["_batteryTemp"]
-        pcb_temp = state["_boardTemp"]
-        errors = state["_cameraError"]
+      # Get the battery's SoC, temperature, PCB temperature, GPS information
+      # and camera errors
+      state = retry(rt, rt.get_state, reconnect_tries = reconnect_tries)
 
-        if errors:
-          errors = ",".join(errors)
-        else:
-          errors = "/"
+      batt_percent = state["batteryLevel"] * 100
+      batt_state = state["_batteryState"]
+      batt_temp = state["_batteryTemp"]
+      pcb_temp = state["_boardTemp"]
+
+      try:
+
+        gps_info = state["_internalGpsInfo"]["gpsInfo"]
+        latitude = gps_info["lat"]
+        longitude = gps_info["lng"]
+        altitude = gps_info["_altitude"]
+        datetimezone = gps_info["_dateTimeZone"]
+        m = gps_datetimezone_regex.match(datetimezone)
+        assert m
+        now = datetime.fromisoformat(
+				"{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}{}".\
+					format(int(m[1]), int(m[2]), int(m[3]),
+						int(m[4]), int(m[5]), int(m[6]),
+						m[7]))
+
+      except:
+        gps_info = None
+
+      errors = state["_cameraError"]
+
+      if errors:
+        errors = ",".join(errors)
+      else:
+        errors = "/"
         
-        log(INFO, "Batt {:0.0f}% ({}), {:0.1f}C - PCB {:0.1f}C - Errors: {}".
+      log(INFO, "Batt {:0.0f}% ({}), {:0.1f}C - PCB {:0.1f}C - Errors: {}".
 			format(batt_percent, batt_state, batt_temp, pcb_temp,
 				errors))
+
+      # If we have GPS data, try to calculate the nautical dawn and dusk times
+      # for today and tomorrow - which may fail in high latitudes when the sun
+      # never rises or never sets
+      if gps_info:
+
+        observer = Observer(latitude=latitude,
+				longitude=longitude,
+				elevation = altitude)
+
+        tomorrow = now + timedelta(days = 1)
+
+        # try to calculate the nautical dawn and dusk times for today and
+        # tomorrow - which may fail in high latitudes when the sun never rises
+        # or never sets
+        try:
+          today_dawn_time = dawn(observer,
+					date = now.date(),
+          depression = twilight_depression,
+					tzinfo = now.tzinfo)
+
+          today_dusk_time = dusk(observer,
+					date = now.date(),
+					depression = twilight_depression,
+					tzinfo = now.tzinfo)
+
+          tomorrow_dawn_time = dawn(observer,
+					date = tomorrow.date(),
+					depression = twilight_depression,
+					tzinfo = tomorrow.tzinfo)
+
+          # Determine whether it's daytime or nighttime, and what time the next
+          # dusk or dawn is
+          if now < today_dawn_time:
+            is_daytime = False
+            next_dawn_dusk_time = today_dawn_time
+
+          elif now < today_dusk_time:
+            is_daytime = True
+            next_dawn_dusk_time = today_dusk_time
+
+          else:
+            is_daytime = False
+            next_dawn_dusk_time = tomorrow_dawn_time
+
+          log(INFO, "GPS time {:%Y-%m-%d %H:%M:%S} ({}time)".
+			format(now,
+				"day" if is_daytime else "night"))
+          log(INFO, "Next {}: {:%Y-%m-%d %H:%M:%S}".
+			format("dusk" if is_daytime else "dawn",
+				next_dawn_dusk_time))
+
+        # Calculating dusk and dawn times failed: determine whether it's
+        # daytime or nighttime by checking the elevation of the sun
+        except:
+          is_daytime = elevation(observer, now) >= twilight_depression
+
+          log(INFO, "GPS time {:%Y-%m-%d %H:%M:%S} ({}time)".
+			format(now,
+				"day" if is_daytime else "night",
+				"dusk" if is_daytime else "dawn"))
+
+        # If we don't know whether the camera is setup for day or night shots,
+        # or if it's set for the wrong time of the day, reconfigure it
+        if is_daytime:
+          if camera_set_for_daytime is None or not camera_set_for_daytime:
+            configure_camera_for_daytime_capture(rt)
+            camera_set_for_daytime = True
+
+        else:	# nighttime
+          if camera_set_for_daytime is None or camera_set_for_daytime:
+            configure_camera_for_nighttime_capture(rt)
+            camera_set_for_daytime = False
 
       # Take a shot
       log(INFO, "Taking a photo")
